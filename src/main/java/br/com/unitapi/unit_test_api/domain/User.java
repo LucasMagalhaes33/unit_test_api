bfa329276148cd9ff41329fea3b361aedfd3e949
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2023-05-14 17:39:53 -0300

    add User class to domain

---
+++ src/main/java/br/com/unitapi/unit_test_api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.unitapi.unit_test_api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
